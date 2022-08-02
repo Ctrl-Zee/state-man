ec3a6a43cbbefd243f03c860d50a503f2a57a1e6
andrew_smith_moser <andrew.smith@moserit.com>
2022-08-02 10:15:24 -0400

    Added header with search and shopping cart

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { HeaderComponent } from './components/header/header.component';
 // Prime NG
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, CardModule, ButtonModule],
-  exports: [HeaderComponent, CardModule, ButtonModule],
+  imports: [CommonModule, CardModule, ButtonModule, InputTextModule],
+  exports: [HeaderComponent, CardModule, ButtonModule, InputTextModule],
 })
 export class SharedModule {}
